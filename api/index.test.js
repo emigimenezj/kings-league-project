25059c01d7f441008da70f2b509eb581026c2b4a
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-10 23:30:18 -0300

    Add new e2e tests for API endpoints

--- api/index.test.js
+++ api/index.test.js
@@ -1,24 +1,84 @@
-import { unstable_dev } from "wrangler";
-import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { unstable_dev } from 'wrangler';
+import { describe, expect, it, beforeAll, afterAll } from 'vitest';
 
-describe("Worker", () => {
+describe('testing / route', () => {
 	let worker;
 
 	beforeAll(async () => {
-		worker = await unstable_dev("src/index.js", {
-			experimental: { disableExperimentalWarning: true },
-		});
+		worker = await unstable_dev(
+			'api/index.js',
+			{ experimental: { disableExperimentalWarning: true }},
+		);
 	});
 
 	afterAll(async () => {
 		await worker.stop();
 	});
 
-	it("should return Hello World", async () => {
-		const resp = await worker.fetch();
-		if (resp) {
-			const text = await resp.text();
-			expect(text).toMatchInlineSnapshot(`"Hello World!"`);
+	it('all routes should have endpoint and description', async () => {
+		const response = await worker.fetch();
+		if (response) {
+			const apiRoutes = await response.json();
+			apiRoutes.forEach(endpoint => {
+				expect(endpoint).toHaveProperty('endpoint');
+				expect(endpoint).toHaveProperty('description');
+			});
+			expect(response.status).toEqual(200);
+		}
+	});
+});
+
+describe('testing /teams routes', () => {
+	let worker;
+
+	beforeAll(async () => {
+		worker = await unstable_dev(
+			'api/index.js',
+			{ experimental: { disableExperimentalWarning: true }},
+		);
+	});
+
+	afterAll(async () => {
+		await worker.stop();
+	});
+
+	const checkProps = team => {
+		expect(team).toHaveProperty('id');
+		expect(team).toHaveProperty('name');
+		expect(team).toHaveProperty('image');
+		expect(team).toHaveProperty('url');
+		expect(team).toHaveProperty('presidentId');
+		expect(team).toHaveProperty('channel');
+		expect(team).toHaveProperty('coach');
+		expect(team).toHaveProperty('socialNetworks');
+		expect(team).toHaveProperty('players');
+	}
+
+	it('all teams should have all props', async () => {
+		const response = await worker.fetch('/teams');
+		if (response) {
+			const teams = await response.json();
+			teams.forEach(team => checkProps(team));
+			expect(response.status).toEqual(200);
+		}
+	});
+
+	it('endpoint for a particular team should have all props', async () => {
+		const response = await worker.fetch('/teams/1k');
+		if (response) {
+			const team = await response.json();
+			checkProps(team);
+			expect(response.status).toEqual(200);
+		}
+	});
+
+	it ('endpoint should return 404 message error for missing teams', async () => {
+		const response = await worker.fetch('/teams/does_not_exist');
+		if (response) {
+			const errorMsg = await response.json();
+
+			expect(errorMsg).toEqual({message: 'Team not found'});
+			expect(response.status).toEqual(404);
 		}
 	});
 });
